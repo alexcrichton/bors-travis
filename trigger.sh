378dbc38ee0241a9425cc04b06146e8e346d64e4
Alex Crichton <alex@alexcrichton.com>
2014-08-01 11:34:48 -0700

    Use master and explicitly specify a build

--- trigger.sh
+++ trigger.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
 for repo in $(cat list); do
-	travis restart -r $repo
+        build=$(travis history -r $repo -b master -l 1 | \
+                awk '{print $1}' | sed 's/#//')
+	travis restart -r $repo $build
 done
